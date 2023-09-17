99cbaf574ddd229db8250d5557b1057ff0211181
cyubimanyo <andhiny.aye@gmail.com>
2023-09-17 18:26:52 +0700

    add 1 test case for login feature

--- cypress/e2e/tricentis-web-shop/login.cy.js
+++ cypress/e2e/tricentis-web-shop/login.cy.js
@@ -1,4 +1,4 @@
-describe('Login with n Test Cases', () => {
+describe('Login with 3 Test Cases', () => {
     beforeEach(() => {
       cy.visit('/')
     })
@@ -12,12 +12,19 @@ describe('Login with n Test Cases', () => {
         })
       })
     
-      it('Login with Random Data', () => {
-        // Read JSON file random account
-        cy.readFile('cypress/fixtures/randomRegister.json').then((account) => {
-          cy.get('.ico-login').should('be.visible').click()
-          cy.login(account.email, account.password)
-          cy.log('Login Berhasil')
-        })
+    it('Login with Random Data', () => {
+      // Read JSON file random account
+      cy.readFile('cypress/fixtures/randomRegister.json').then((account) => {
+        cy.get('.ico-login').should('be.visible').click()
+        cy.login(account.email, account.password)
+        cy.log('Login Berhasil')
       })
+    })
+
+    it('Login with All Fields Blank and should validate field error messages', () => {
+      cy.get('.ico-login').should('be.visible').click()
+      cy.get('form > .buttons > .button-1').should('be.visible').click()
+      cy.get('.validation-summary-errors').contains('Login was unsuccessful. Please correct the errors and try again.')
+      cy.get('.validation-summary-errors').contains('No customer account found')
+    })
 })
